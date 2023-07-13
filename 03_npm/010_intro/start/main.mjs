a1ee2b01a8069dc01e4e265a755388508a5efb1c
Kazuhiro Takahashi <num96kazu19@icloud.com>
2023-07-13 23:35:06 +0900

    npmについて

--- 03_npm/010_intro/start/main.mjs
+++ 03_npm/010_intro/start/main.mjs
@@ -1,2 +1,9 @@
+import _ from "lodash";
+
 const original = { prop: { nested: "value" } };
 // オブジェクトの複製
+
+const cloned = _.cloneDeep(original);
+cloned.prop.nested = "new";
+
+console.log(cloned, original);
